06a9f3f6be7465de15662573cd4f165786c6d083
gioigioi124 <gioigioi12.4@gmail.com>
2025-08-31 17:44:56 +0700

    3

--- test.js
+++ test.js
@@ -542,6 +542,67 @@
 // let currentDate = new Date();
 // console.log(currentDate);
 
-const myDate1 = new Date(2025, 7, 29);
-console.log(myDate1);
-console.log(myDate1.toLocaleDateString());
+// const myDate1 = new Date(2025, 7, 29);
+// console.log(myDate1);
+// console.log(myDate1.toLocaleDateString());
+
+// let currentDate = new Date();
+// function birthYearValid(birthYear) {
+//   let year = Number(prompt(`Mời nhập năm sinh:`));
+//   if (year <= 0 || !Number.isInteger(year)) {
+//     alert(`Tuổi phải lớn hơn 0
+//     Tuổi phải là số nguyên dương`);
+//     return birthYearValid();
+//   } else {
+//     birthYear = currentDate.getFullYear() - year;
+//     return birthYear;
+//   }
+// }
+// alert(birthYearValid());
+
+// function countDown(minutes) {
+//   alert(`Thời gian làm bài của bạn là ${minutes} phút`);
+//   let seconds = minutes * 60;
+//   let countDownInterval = setInterval(function () {
+//     // thân hàm
+//     // làm tròn xuống số phút
+//     let minutesLeft = Math.floor(seconds / 60);
+//     let secondsLeft = seconds % 60;
+//     console.log(`${minutesLeft}:${secondsLeft}`);
+//     if (seconds <= 0) {
+//       clearInterval(countDownInterval);
+//       alert(`Hết thời gian`);
+//     } else {
+//       seconds--;
+//     }
+//   }, 1000);
+// }
+// let timeInput = Number(prompt(`Mời nhập thời gian làm bài:`));
+// countDown(timeInput);
+
+// function timeSince(timestamp) {
+//   let now = new Date().getTime();
+//   // const offlineTime = 1756630276413;
+//   let timeDif = now - timestamp;
+//   let seconds = Math.floor(timeDif / 1000);
+//   let minutes = Math.floor(seconds / 60);
+//   let hours = Math.floor(minutes / 60);
+//   let days = Math.floor(hours / 24);
+//   let months = Math.floor(days / 30);
+//   let years = Math.floor(months / 12);
+//   if (years > 0) {
+//     return `Online ${years} năm trước`;
+//   } else if (months > 0) {
+//     return `Online ${months} tháng trước`;
+//   } else if (days > 0) {
+//     return `Online ${days} ngày trước`;
+//   } else if (hours > 0) {
+//     return `Online ${hours} giờ trước`;
+//   } else if (minutes > 0) {
+//     return `Online ${minutes} phút trước`;
+//   } else {
+//     return `Online ${seconds} giây trước`;
+//   }
+// }
+// let timestamp = prompt("Nhập timeStamp");
+// console.log(timeSince(timestamp));
